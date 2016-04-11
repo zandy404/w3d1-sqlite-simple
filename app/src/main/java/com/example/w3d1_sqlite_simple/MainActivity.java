f665ad6e161e8fb8eb5b140fd90ff142d7b6eeb8
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-11 13:28:19 -0400

    Add insertion logic

--- app/src/main/java/com/example/w3d1_sqlite_simple/MainActivity.java
+++ app/src/main/java/com/example/w3d1_sqlite_simple/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.w3d1_sqlite_simple;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EXAMPLE_NAME = "Edwin";
+    private static final String EXAMPLE_AGE = "31";
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doMagic(View view) {
+
+        UsersDatabaseHelper usersDatabaseHelper = new UsersDatabaseHelper(getApplicationContext());
+        SQLiteDatabase db = usersDatabaseHelper.getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(UsersDatabaseHelper.KEY_USER_NAME, EXAMPLE_NAME);
+            values.put(UsersDatabaseHelper.KEY_AGE, EXAMPLE_AGE);
+
+            db.insertOrThrow(UsersDatabaseHelper.TABLE_USERS, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            Log.e(TAG, "Error while trying to add post to database");
+        } finally {
+            db.endTransaction();
+        }
     }
 }
