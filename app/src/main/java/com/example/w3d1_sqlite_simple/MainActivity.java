7d8358b0b5729240f3bbc0b0919caa92db8ac983
zandy404 <zandy.lin@gmail.com>
2016-07-18 10:54:09 -0400

    Second commit

--- app/src/main/java/com/example/w3d1_sqlite_simple/MainActivity.java
+++ app/src/main/java/com/example/w3d1_sqlite_simple/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.w3d1_sqlite_simple;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -39,5 +40,64 @@ public class MainActivity extends AppCompatActivity {
         } finally {
             db.endTransaction();
         }
+    }
+
+    public void readMagic(View view) {
+        Log.d(TAG, "doMagic: ");
+        UsersDatabaseHelper usersDatabaseHelper = new UsersDatabaseHelper(getApplicationContext());
+        SQLiteDatabase db = usersDatabaseHelper.getWritableDatabase();
+
+        final String POSTS_SELECT_QUERY = "SELECT * from " + UsersDatabaseHelper.TABLE_USERS;
+        Cursor cursor = db.rawQuery(POSTS_SELECT_QUERY, null);
+
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    String nameUser = cursor.getString(cursor.getColumnIndex(UsersDatabaseHelper.KEY_USER_NAME));
+                    String ageUser = cursor.getString(cursor.getColumnIndex(UsersDatabaseHelper.KEY_USER_ID));
+
+                    Log.d(TAG, "logInformation: " + nameUser);
+                    Log.d(TAG, "logInformation: " + ageUser);
+
+                } while (cursor.moveToNext());
+            }
+
+        } catch (Exception e) {
+            Log.e(TAG, "Error while trying to add post to database");
+        } finally {
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+        }
+    }
+
+    public void readData(View view) {
+        UsersDatabaseHelper usersDatabaseHelper = new UsersDatabaseHelper(getApplicationContext());
+        SQLiteDatabase db = usersDatabaseHelper.getWritableDatabase();
+
+        final String POSTS_SELECT_QUERY = "SELECT * from " + UsersDatabaseHelper.TABLE_USERS;
+        Cursor cursor = db.query(UsersDatabaseHelper.TABLE_USERS,
+                null, //columns
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        cursor.moveToFirst();
+
+        do {
+
+            Log.d(TAG, "ReadInformation: " + cursor.getString(cursor.getColumnIndex(UsersDatabaseHelper.KEY_USER_ID)));
+            Log.d(TAG, "ReadInformation: " + cursor.getString(cursor.getColumnIndex(UsersDatabaseHelper.KEY_USER_NAME)));
+            Log.d(TAG, "ReadInformation: " + cursor.getString(cursor.getColumnIndex(UsersDatabaseHelper.KEY_AGE)));
+
+        } while (cursor.moveToNext());
+
+        cursor.close();
+    }
+
+    public void readMagicCursor(View view) {
+        readData(view);
     }
 }
